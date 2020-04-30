69b51e00687c8e68fc790b8af1b045b864c6b390
santosh <santospandey333@gmail.com>
2020-04-30 15:30:17 +0545

    added dictionary

--- app/controllers/game_container_controller.rb
+++ app/controllers/game_container_controller.rb
@@ -1,2 +1,14 @@
 class GameContainerController < ApplicationController
+    def initialize
+        @words = {}
+        File.open("./dictionary.txt") do |file|
+            file.each do |line|
+                @words[line.strip] = true
+            end
+        end
+    end
+    def check_word
+        word = params[:word]
+        
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root "game_container#index"
+  get "word/:word", to:"game_container#check_word"
 end
